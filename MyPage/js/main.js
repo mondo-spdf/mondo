ae562d0192d327f66531a12e5faed526ce704961
mondo-spdf <68699906+mondo-spdf@users.noreply.github.com>
2020-09-10 22:54:55 +0900

    Add files via upload

---
+++ MyPage/js/main.js
@@ -0,0 +1,92 @@
+'use strict'
+{
+  const siteSection = document.getElementById("siteSection");
+  const sites =
+  [
+    {title: "東海オンエアクイズ！ | もんどのプログラミング", img: "photo/site1.png", date: "20/07/07", detail: "東海オンエアの動画の一部分の音だけを聞いて、それが一体なんの動画なのか、選択肢の中から当てるゲームです。", tag: ["html", "css", "javascript"], url: "https://mondo-spdf.github.io/MyTOKAIONAIR/MyTOKAIONAIR/"},
+    {title: "オススメゲーム紹介 | もんどのプログラミング", img: "photo/site2.png", date: "20/08/08", detail: "友達に一緒にやるPCゲームを紹介するために作ったサイトです。背景に動画を使っているサイトを作りたくてチャレンジしました。",  tag: ["html", "css", "javascript"]},
+  ];
+
+  function SearchTag(site, tags) {
+    for(let i = 0; i < site.tag.length; i++) {
+      switch(site.tag[i]) {
+        case "html":
+        const html = document.createElement('p');
+        html.classList.add('tag');
+        html.classList.add('html');
+        html.textContent = site.tag[i];
+        tags.appendChild(html);
+        break;
+        
+        case "css":
+        const css = document.createElement('p');
+        css.classList.add('tag');
+        css.classList.add('css');
+        css.textContent = site.tag[i];
+        tags.appendChild(css);
+        break;
+
+        case "javascript":
+        const javascript = document.createElement('p');
+        javascript.classList.add('tag');
+        javascript.classList.add('javascript');
+        javascript.textContent = site.tag[i];
+        tags.appendChild(javascript);
+        break;
+
+        case "ruby":
+        const ruby = document.createElement('p');
+        ruby.classList.add('tag');
+        ruby.classList.add('ruby');
+        ruby.textContent = site.tag[i];
+        tags.appendChild(ruby);
+        break;
+
+        case "Node.js":
+        const Node = document.createElement('p');
+        Node.classList.add('tag');
+        Node.classList.add('Node.js');
+        Node.textContent = site.tag[i];
+        tags.appendChild(Node);
+        break;
+      }
+    }
+  }
+
+  function Setsites() {
+    sites.forEach(site => {
+      const siteContent = document.createElement('div');
+      siteContent.classList.add('site');
+      siteSection.appendChild(siteContent);
+      const a = document.createElement('a');
+      a.target = "_blank";
+      a.href = site.url;
+      siteContent.appendChild(a);
+
+      const img = document.createElement('img');
+      img.src = site.img;
+      a.appendChild(img);
+      const title = document.createElement('h1');
+      title.textContent = site.title;
+      a.appendChild(title);
+      const date = document.createElement('p');
+      date.classList.add('date');
+      date.textContent = `作成日：${site.date}`;
+      a.appendChild(date);
+      const detail = document.createElement('p');
+      detail.classList.add('detail');
+      detail.textContent = site.detail;
+      a.appendChild(detail);
+
+      const tags = document.createElement('div');
+      tags.classList.add('tags');
+      a.appendChild(tags);
+
+      SearchTag(site, tags);
+    });
+    
+  }
+
+
+  Setsites();
+}
