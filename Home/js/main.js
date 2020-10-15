ece5d6220ade322088a53e2a8ff6891a73003abf
mondo-spdf <mondo1012daiya1012@gmail.com>
2020-10-15 23:39:04 +0900

    create homepage.

---
+++ Home/js/main.js
@@ -0,0 +1,17 @@
+$('img').hide().fadeIn(2500);
+$('.logo').removeClass('logo-start');
+let exit_count = 0;
+let elems_count = 0;
+function repeat_show(elems$) {
+  elems_count = elems$.length;
+  elems$.eq(exit_count).show(300, () => {
+    exit_count ++;
+    if(exit_count < elems_count) {
+      repeat_show(elems$);
+    }
+  });
+}
+$("li").delay(2200);
+
+// repeat_show($("li"));
+$("li").hide().fadeIn(1600);
