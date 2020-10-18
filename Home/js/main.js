7c873d241ce87c8790a5497cd8e66895aa4c961d
mondo-spdf <mondo1012daiya1012@gmail.com>
2020-10-18 15:49:29 +0900

    updade home.(delete searchbar)

--- Home/js/main.js
+++ Home/js/main.js
@@ -2,20 +2,4 @@
 
 $('img').hide().fadeIn(2500);
 $('.logo').removeClass('logo-start');
-let exit_count = 0;
-let elems_count = 0;
-function repeat_show(elems$) {
-  elems_count = elems$.length;
-  elems$.eq(exit_count).show(300, () => {
-    exit_count ++;
-    if(exit_count < elems_count) {
-      repeat_show(elems$);
-    }
-  });
-}
-$("li").delay(2200);
-
-// repeat_show($("li"));
-$("li").hide().fadeIn(1600);
-
-document.getElementById('search').focus();
+$("li").delay(2200).hide().fadeIn(1600);
