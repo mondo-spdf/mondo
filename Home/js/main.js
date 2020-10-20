90756648d4b2c12b2d5765dee839cfbed7f48a8d
mondo-spdf <mondo1012daiya1012@gmail.com>
2020-10-21 08:41:44 +0900

    css changes with time.

--- Home/js/main.js
+++ Home/js/main.js
@@ -3,3 +3,14 @@
 $('img').hide().fadeIn(2500);
 $('.logo').removeClass('logo-start');
 $("li").delay(2200).hide().fadeIn(1600);
+
+$(() => {
+  let hour = new Date().getHours();
+  if(hour <= 5 || hour >= 18) {
+    $('body').removeClass('white');
+    $('body').addClass('black');
+  } else {
+    $('body').removeClass('black');
+    $('body').addClass('white');
+  }
+});
